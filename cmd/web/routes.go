2a68d0f248aa14fb5bb2e5217ca0d1e5effed400
Bayvao <vermabayvao@gmail.com>
2023-11-18 17:43:30 +0530

    Adding images

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,7 +17,6 @@ import (
 // 	return mux
 // }
 
-
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
@@ -28,5 +27,9 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
